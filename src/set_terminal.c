618752e9af376e771b9ef8a5a671de91be92ff72
Dkroeke <Dkroeke@gmail.com>
2020-02-16 16:39:31 +0100

    -add terminal header with terminfo defines

--- src/set_terminal.c
+++ src/set_terminal.c
@@ -10,7 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell21.h"
+#include "terminal.h"
+
+static int	ft_putchar(int c)
+{
+	return (write(STDERR_FILENO, &c, 1));
+}
+
+void		send_terminal(char *command)
+{
+	tputs(tgetstr(command, NULL), 1, ft_putchar);
+}
 
 void		configure_terminal(t_shell *shell, int activator)
 {
@@ -18,14 +28,20 @@ void		configure_terminal(t_shell *shell, int activator)
 
 	if (activator == 1)
 	{
+		tgetent(NULL, getenv("TERM"));
 		tcgetattr(STDIN_FILENO, &orig);
 		shell->term = orig;
-		shell->term.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-		shell->term.c_iflag &= ~(IXON);
+		shell->term.c_lflag &= ~(ECHO | ICANON);
+		//shell->term.c_iflag &= ~(IXON);
 		shell->term.c_cc[VMIN] = 0;
 		shell->term.c_cc[VTIME] = 1;
 		tcsetattr(STDIN_FILENO, TCSAFLUSH, &shell->term);
 	}
 	if (activator == 0)
 		tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig);
+}
+
+void		get_winsize(t_shell *shell)
+{
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &shell->winsize);
 }
