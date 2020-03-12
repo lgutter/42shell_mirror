0d76482d327a08f88e8066baae85f38a07e2846a
Liewe Gutter <lgutter@student.codam.nl>
2020-03-12 16:03:11 +0100

    ADD global internal error + pointer error functions
    error codes will now be tracked in a global variable
    called g_error_internal. This makes it possible to check it at any time,
    and to set it without having to chain return values, or use pointers.
    Also added 2 version of the handle_error functions that will take and
    return a pointer. This is useful in functions that need to return but
    don't return an int. it can also be cast to and from a void * if it is
    something different from a void pointer.
    also improved text wrapping in function comments in the header.

--- inc/handle_error.h
+++ inc/handle_error.h
@@ -16,11 +16,18 @@
 # include "ft_printf.h"
 
 /*
-**	enumerations used to pass error codes between functions.
+**	global variable used to store the most recent internal error code.
+**	this can be used to check an error state. After the error state is
+**	handled, it should be set back to 0.
+*/
+int					g_error_internal;
+
+/*
+**	enumerations used to indentify error codes easier.
 */
 typedef enum		e_error
 {
-	unknown_error = 0,
+	no_error,
 	malloc_error,
 	error_count
 }					t_error;
@@ -30,26 +37,54 @@ typedef enum		e_error
 **	these strings will be used to print a relevant error message.
 */
 static const char	*g_error_str[error_count] = {
-	[unknown_error] = "An unknown error occurred",
+	[no_error] = "Error code indicates no error",
 	[malloc_error] = "Malloc failed to allocate memory",
 };
 
 /*
-**	handle_error takes an error_code (see error enums), and prints the relevant
-**	error message, after which it returns the error code again.
+**	handle_error takes an error_code (see error enums), and prints the
+**	relevant error message, after which it returns the error code again.
+**
 **	arg: error_code: the error code as can be found in the error enumerations.
 **	returns: the error code given as argument.
 */
 int		handle_error(int error_code);
 
 /*
-**	handle_error_str takes an error_code (see error enums) and string as args,
-**	and prints the relevant error message followed by the string,
-**	after which it returns the error code again.
+**	handle_error_str takes an error_code (see error enums) and string
+**	as args, and prints the relevant error message followed by the
+**	string, after which it returns the error code again.
+**
 **	arg: error_code: the error code as can be found in the error enumerations.
 **	arg: str: the string to be printed after the error message.
 **	returns: the error code given as argument.
 */
 int		handle_error_str(int error_code, const char *str);
+
+/*
+**	handle_error_str_p takes an error_code (see error enums), a string,
+**	and a pointer as args, and prints the relevant error message
+**	followed by the string, after which it returns the pointer given
+**	as argument. This function is useful to call in a return statement
+**	when the function it is called from returns something different than an int.
+**
+**	arg: error_code: the error code as can be found in the error enumerations.
+**	arg: str: the string to be printed after the error message.
+**	returns: the error code given as argument.
+*/
+void	*handle_error_str_p(int error_code, const char *str, void *pointer);
+
+/*
+**	handle_error_p takes an error_code (see error enums) and a pointer
+**	as args, and prints the relevant error message, after which it
+**	returns the pointer given as argument. This function is useful to
+**	call in a return statement when the function it is called from
+**	returns something different than an int.
+**
+**	arg: error_code: the error code as can be found in the error enumerations.
+**	arg: pointer: the pointer to be returned after the error message is printed.
+**	returns: the error code given as argument.
+*/
+void	*handle_error_p(int error_code, void *pointer);
 
 #endif
--- src/handle_error.c
+++ src/handle_error.c
@@ -14,12 +14,28 @@
 
 int		handle_error(int error_code)
 {
+	g_error_internal = error_code;
 	ft_dprintf(STDERR_FILENO, "%s\n", g_error_str[error_code]);
 	return (error_code);
 }
 
 int		handle_error_str(int error_code, const char *str)
 {
+	g_error_internal = error_code;
 	ft_dprintf(STDERR_FILENO, "%s: %s\n", g_error_str[error_code], str);
 	return (error_code);
+}
+
+void	*handle_error_str_p(int error_code, const char *str, void *pointer)
+{
+	g_error_internal = error_code;
+	ft_dprintf(STDERR_FILENO, "%s: %s\n", g_error_str[error_code], str);
+	return (pointer);
+}
+
+void	*handle_error_p(int error_code, void *pointer)
+{
+	g_error_internal = error_code;
+	ft_dprintf(STDERR_FILENO, "%s\n", g_error_str[error_code]);
+	return (pointer);
 }
