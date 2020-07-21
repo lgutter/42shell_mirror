cab19cb931da04d740cac033a302a6e501f7c9e1
Liewe Gutter <lgutter@student.codam.nl>
2020-07-21 17:23:13 +0200

    restored execute_builtin to old version
    restored execute_builtin to the old version that sets status,
    and ignores assignment words.

--- src/builtins/builtins_1.c
+++ src/builtins/builtins_1.c
@@ -44,44 +44,30 @@ int					execute_builtin(t_shell *shell, char **argv)
 {
 	int		ret;
 	int		i;
-	char	**new_argv;
 
 	ret = 0;
 	i = 0;
-	new_argv = NULL;
-	if (ft_countchar(argv[0], '=') == 1)
-		new_argv = update_argv(argv);
-	if (shell == NULL || argv == NULL || argv[0] == NULL)
+	if (argv == NULL || argv[0] == NULL)
 		return (-1);
 	while (g_builtins[i].builtin_name != NULL)
 	{
-		if (ft_strcmp((new_argv == NULL ? argv[0] : new_argv[0]),
-				g_builtins[i].builtin_name) == 0)
+		if (ft_strcmp(argv[0], g_builtins[i].builtin_name) == 0)
 		{
-			ret = ((new_argv != NULL) ? g_builtins[i].f(shell, new_argv)
-										: g_builtins[i].f(shell, argv));
-			new_argv = NULL;
+			ret = g_builtins[i].f(shell, argv);
+			if (shell != NULL)
+				ft_setstatus(shell->env, ret);
+			return (ret);
 		}
 		i++;
 	}
-	free_dchar_arr(new_argv);
-	return (ret);
+	return (-1);
 }
 
 int					is_builtin(char *exec_name)
 {
 	size_t				i;
-	char				**split;
 
 	i = 0;
-	split = ft_strsplit(exec_name, '=');
-	if (split != NULL && split[0] != NULL && split[1] != NULL
-		&& split[2] == NULL)
-	{
-		free_dchar_arr(split);
-		return (1);
-	}
-	free_dchar_arr(split);
 	while (g_builtins[i].builtin_name != NULL)
 	{
 		if (ft_strcmp(g_builtins[i].builtin_name, exec_name) == 0)
