191808b7a74f57e920c5aec9535529fe2b1cdf2f
Liewe Gutter <lgutter@student.codam.nl>
2020-07-25 14:22:51 +0200

    fix type builtin and add tests
    the type builtin now correctly recognizes directories, and returns the
    expected value.
    also added tests.

--- src/builtins/builtin_type.c
+++ src/builtins/builtin_type.c
@@ -12,28 +12,59 @@
 
 #include "builtins.h"
 
+static bool	is_executable(char *path)
+{
+	struct stat statbuff;
+
+	if (stat(path, &statbuff) != 0)
+		return (false);
+	if (access(path, X_OK) != 0)
+		return (false);
+	return (S_ISREG(statbuff.st_mode));
+}
+
+static int	type_executable(t_shell *shell, char *path)
+{
+	char	*check;
+	int		ret;
+
+	ret = 0;
+	check = NULL;
+	find_executable(shell == NULL ? NULL : shell->env, &check, path);
+	if (check != NULL &&
+			(ft_strchr(check, '/') == NULL || is_executable(check) == true))
+	{
+		ft_printf("%s is %s\n", path, check);
+		ret = 0;
+	}
+	else
+	{
+		ft_printf("%s not found\n", path);
+		ret = 1;
+	}
+	free(check);
+	return (ret);
+}
+
 int			builtin_type(t_shell *shell, char **argv)
 {
 	size_t	i;
-	char	*check;
+	int		ret;
 
+	if (argv == NULL || argv[0] == NULL)
+		return (1);
+	ret = 0;
 	i = 1;
 	while (argv[i] != NULL)
 	{
-		if (is_builtin(argv[i]) == 1)
+		if (is_builtin(argv[i]) == true)
 			ft_printf("%s is a shell builtin\n", argv[i]);
 		else
 		{
-			if (find_executable(shell->env, &check, argv[i]) != 0
-				&& access(check, X_OK) == 0)
-				ft_printf("%s is %s\n", argv[i], argv[i]);
-			else if (access(check, X_OK) == 0)
-				ft_printf("%s is %s\n", argv[i], check);
-			else
-				ft_printf("%s not found\n", argv[i]);
-			free(check);
+			if (type_executable(shell, argv[i]) != 0)
+				ret = 1;
 		}
 		i++;
 	}
-	return (0);
+	return (ret);
 }
---
+++ src/builtins/builtin_type.spec.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   builtin_type.spec.c                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/07/25 13:59:40 by lgutter       #+#    #+#                 */
+/*   Updated: 2020/07/25 13:59:40 by lgutter       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include <criterion/assert.h>
+
+#include "builtins.h"
+#include "environment.h"
+#include "error_str.h"
+
+static void redirect_both()
+{
+	cr_redirect_stdout();
+	cr_redirect_stderr();
+}
+
+Test(builtin_type_unit, invalid_NULL_argv)
+{
+	cr_expect_eq(builtin_type(NULL, NULL), 1);
+}
+
+Test(builtin_type_unit, invalid_NULL_argv_zero)
+{
+	char	*null = NULL;
+	char	**argv = &null;
+
+	cr_expect_eq(builtin_type(NULL, argv), 1);
+}
+
+Test(builtin_type_unit, valid_NULL_shell_find_builtin, .init = cr_redirect_stdout)
+{
+	char	**argv = ft_strsplit_t_s("type echo");
+	char	*expected_output = "echo is a shell builtin\n";
+
+	cr_expect_eq(builtin_type(NULL, argv), 0);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+}
+
+Test(builtin_type_unit, invalid_NULL_shell_find_ls, .init = redirect_both)
+{
+	char	**argv = ft_strsplit_t_s("type ls");
+	char	*expected_output = "ls not found\n";
+
+	char	expected_error[1024];
+	memset(expected_error, '\0', 1024);
+	snprintf(expected_error, 1024, "Cetushell: %s: PATH\n", g_error_str[env_not_found]);
+	cr_expect_eq(builtin_type(NULL, argv), 1);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+	fflush(stderr);
+	cr_expect_stderr_eq_str(expected_error);
+}
+
+Test(builtin_type_unit, valid_find_builtin, .init = cr_redirect_stdout)
+{
+	t_shell	*shell = init_shell(false);
+	char	**argv = ft_strsplit_t_s("type exit");
+	char	*expected_output = "exit is a shell builtin\n";
+
+	cr_expect_eq(builtin_type(shell, argv), 0);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+}
+
+Test(builtin_type_unit, valid_find_command, .init = cr_redirect_stdout)
+{
+	t_shell	*shell = init_shell(false);
+	char	**argv = ft_strsplit_t_s("type more");
+	char	*expected_output = "more is /usr/bin/more\n";
+
+	cr_expect_eq(builtin_type(shell, argv), 0);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+}
+
+Test(builtin_type_unit, valid_find_both, .init = cr_redirect_stdout)
+{
+	t_shell	*shell = init_shell(false);
+	char	**argv = ft_strsplit_t_s("type more exit");
+	char	*expected_output = "more is /usr/bin/more\nexit is a shell builtin\n";
+
+	cr_expect_eq(builtin_type(shell, argv), 0);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+}
+
+Test(builtin_type_unit, valid_find_non_existent, .init = cr_redirect_stdout)
+{
+	t_shell	*shell = init_shell(false);
+	char	**argv = ft_strsplit_t_s("type foo");
+	char	*expected_output = "foo not found\n";
+
+	cr_expect_eq(builtin_type(shell, argv), 1);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+}
+
+Test(builtin_type_unit, valid_find_directory, .init = cr_redirect_stdout)
+{
+	t_shell	*shell = init_shell(false);
+	char	**argv = ft_strsplit_t_s("type /tmp");
+	char	*expected_output = "/tmp not found\n";
+
+	cr_expect_eq(builtin_type(shell, argv), 1);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+}
+
+Test(builtin_type_unit, valid_find_all, .init = cr_redirect_stdout)
+{
+	t_shell	*shell = init_shell(false);
+	char	**argv = ft_strsplit_t_s("type more exit foo");
+	char	*expected_output = "more is /usr/bin/more\nexit is a shell builtin\nfoo not found\n";
+
+	cr_expect_eq(builtin_type(shell, argv), 1);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+}
