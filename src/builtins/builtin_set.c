dd6264b17e232e0e95809cbbbec8a841691e0034
Liewe Gutter <lgutter@student.codam.nl>
2020-07-25 13:05:32 +0200

    remove unneeded error print and null checks

--- src/builtins/builtin_set.c
+++ src/builtins/builtin_set.c
@@ -19,10 +19,8 @@ int				builtin_set(t_shell *shell, char **argv)
 	t_env	*head;
 
 	(void)argv;
-	if (shell == NULL || argv == NULL || argv[0] == NULL)
+	if (shell == NULL)
 		return (1);
-	if (ft_str_arr_len(argv) > 1)
-		ft_printf("set: no arguments needed, arguments ignored\n\n");
 	head = shell->env;
 	while (head != NULL)
 	{
