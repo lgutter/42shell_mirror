47a6a4e0c6ecf575f6e9ed849ef9fa9fba918a23
Liewe Gutter <lgutter@student.codam.nl>
2020-07-31 12:17:04 +0200

    fix chain logic for && and || operators
    if the first operator was false, we skipped the entire command list
    instead of just the next command. Because of that, things like
    "false && echo yes || echo no" did not work at all.
    also added 2 tests for this.

--- src/handle_input.spec.c
+++ src/handle_input.spec.c
@@ -161,6 +161,40 @@ Test(handle_input_integration, basic_run_and_or_commands_fail, .init = cr_redire
 	cr_expect_stdout_eq_str(expected_output);
 }
 
+Test(handle_input_integration, basic_run_and_or_commands_fail_first, .init = cr_redirect_stdout)
+{
+	char		*input = "false && echo foo || echo bar";
+	char		*expected_output = "bar\n";
+
+
+
+	t_shell		*shell = init_shell(false);
+	char		*buffer = strdup(input);
+	cr_assert_not_null(shell);
+	cr_assert_not_null(buffer);
+	handle_input(shell, &input);
+	cr_expect_str_eq(buffer, input);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+}
+
+Test(handle_input_integration, basic_run_or_and_commands_succeed_first, .init = cr_redirect_stdout)
+{
+	char		*input = "true || echo foo && echo bar";
+	char		*expected_output = "bar\n";
+
+
+
+	t_shell		*shell = init_shell(false);
+	char		*buffer = strdup(input);
+	cr_assert_not_null(shell);
+	cr_assert_not_null(buffer);
+	handle_input(shell, &input);
+	cr_expect_str_eq(buffer, input);
+	fflush(stdout);
+	cr_expect_stdout_eq_str(expected_output);
+}
+
 Test(handle_input_integration, basic_run_pipe_command_background, .init = cr_redirect_stdout)
 {
 	char		*input = "/bin/echo foo | cat -e &";
