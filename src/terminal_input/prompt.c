lgutter/42shell_mirror

commit 13e6ac017f64cadb3f028705e72e275bfef763db

update output fd of cetushell
we now handle all terminal interaction of the shell itself through
standard error. Interactive mode now also requires stderr to be bound
the terminal. if it is not, we run in non-interactive mode.

src/terminal_input/prompt.c
@@ -21,37 +21,42 @@ static void		print_buffer(t_buff *buffer)
 {
 	if (buffer->rv_end > buffer->rv_start)
 	{
-		ft_printf("%.*s%s", buffer->rv_start, buffer->buff, RV_MODE);
-		ft_printf("%.*s", buffer->rv_end - buffer->rv_start,
+		ft_dprintf(STDERR_FILENO, "%.*s%s", buffer->rv_start,
+														buffer->buff, RV_MODE);
+		ft_dprintf(STDERR_FILENO, "%.*s", buffer->rv_end - buffer->rv_start,
 		&buffer->buff[buffer->rv_start]);
-		ft_printf("%s%s", RV_RESET, &buffer->buff[buffer->rv_end]);
+		ft_dprintf(STDERR_FILENO, "%s%s", RV_RESET,
+												buffer->buff[buffer->rv_end]);
 	}
 	else if (buffer->rv_end < buffer->rv_start)
 	{
-		ft_printf("%.*s%s", buffer->rv_end, buffer->buff, RV_MODE);
-		ft_printf("%.*s", buffer->rv_start - buffer->rv_end,
+		ft_dprintf(STDERR_FILENO, "%.*s%s",
+										buffer->rv_end, buffer->buff, RV_MODE);
+		ft_dprintf(STDERR_FILENO, "%.*s", buffer->rv_start - buffer->rv_end,
 		&buffer->buff[buffer->rv_end]);
-		ft_printf("%s%s", RV_RESET, &buffer->buff[buffer->rv_start]);
+		ft_dprintf(STDERR_FILENO, "%s%s", RV_RESET,
+											&buffer->buff[buffer->rv_start]);
 	}
 	else
-		ft_printf("%s", buffer->buff);
+		ft_dprintf(STDERR_FILENO, "%s", buffer->buff);
 }
 
 static void		refresh_prompt(t_buff *buffer, t_cursor *cursor)
 {
 	if (cursor->new_line_x > 1)
 	{
-		ft_printf("%c[%d;%dH", ESCAPE_KEY, cursor->start.y - 1,
+		ft_dprintf(STDERR_FILENO, "%c[%d;%dH", ESCAPE_KEY, cursor->start.y - 1,
 			cursor->new_line_x);
-		ft_printf("%s%%%s", RV_MODE, RV_RESET);
-		ft_printf("%c[%d;%dH", ESCAPE_KEY, cursor->start.y, cursor->start.x);
+		ft_dprintf(STDERR_FILENO, "%s%%%s", RV_MODE, RV_RESET);
+		ft_dprintf(STDERR_FILENO, "%c[%d;%dH", ESCAPE_KEY, cursor->start.y,
+															cursor->start.x);
 		cursor->new_line_x = 1;
 	}
-	ft_printf("%c[%d;%dH", ESCAPE_KEY, cursor->start.y, 0);
+	ft_dprintf(STDERR_FILENO, "%c[%d;%dH", ESCAPE_KEY, cursor->start.y, 0);
 	send_terminal("cd");
-	ft_printf("%s", buffer->prompt);
+	ft_dprintf(STDERR_FILENO, "%s", buffer->prompt);
 	print_buffer(buffer);
-	ft_printf("%s", cursor->cur_buff);
+	ft_dprintf(STDERR_FILENO, "%s", cursor->cur_buff);
 }
 
 static int		init_buffs(t_buff *buffer, t_cursor *cursor, const char *prompt)

src/autocomplete/auto_complete_lists.c
@@ -17,7 +17,7 @@
 size_t			scroll_cursor(t_cursor *cursor)
 {
 	if (cursor->current.y < cursor->max.y)
-		ft_printf("\n");
+		ft_dprintf(STDERR_FILENO, "\n");
 	cursor->start.y++;
 	cursor->current.y++;
 	if (cursor->current.y > cursor->max.y)
@@ -46,10 +46,11 @@ void			print_complete_list(t_shell *shell, t_complete *comp)
 		if (i >= col)
 			i = scroll_cursor(&shell->cursor);
 		if (comp->options & (VAR_DBRACK | VAR_DOLLAR))
-			ft_printf("%s%-*s ", ((comp->options & VAR_DBRACK) ? "${" : "$"),
-			(comp->max_len), list->match);
+			ft_dprintf(STDERR_FILENO, "%s%-*s ",
+							((comp->options & VAR_DBRACK) ? "${" : "$"),
+												(comp->max_len), list->match);
 		else
-			ft_printf("%-*s ", comp->max_len, list->match);
+			ft_dprintf(STDERR_FILENO, "%-*s ", comp->max_len, list->match);
 		list = list->next;
 		i++;
 	}

src/builtins/builtin_fg.c
@@ -56,7 +56,7 @@ int				builtin_fg(t_shell *shell, char **argv)
 								shell->job_control->previous, job_print_short);
 	job->status = running;
 	status = job->status;
-	tcsetpgrp(STDIN_FILENO, job->pgrp);
+	tcsetpgrp(STDERR_FILENO, job->pgrp);
 	configure_terminal(NULL, 0);
 	kill(-(job->pgrp), SIGCONT);
 	while (status == job->status)
@@ -65,7 +65,7 @@ int				builtin_fg(t_shell *shell, char **argv)
 		status = get_job_status(job);
 	}
 	check_jobs(shell->job_control, job_update_all);
-	tcsetpgrp(STDIN_FILENO, shell->pgid);
+	tcsetpgrp(STDERR_FILENO, shell->pgid);
 	configure_terminal(shell, 3);
 	return (0);
 }

src/executor/exec_pipe_sequence.c
@@ -24,7 +24,7 @@ static void	exec_in_child(t_pipe_sequence *pipe_seq, t_shell *shell,
 	set_process_job_group(job, process);
 	if (job->foreground == true)
 	{
-		tcsetpgrp(STDIN_FILENO, job->pgrp);
+		tcsetpgrp(STDERR_FILENO, job->pgrp);
 		configure_terminal(NULL, 0);
 	}
 	reset_signals();
@@ -135,6 +135,6 @@ int			exec_pipe_sequence(t_pipe_sequence *pipe_seq,
 		ret = execute_simple(pipe_seq, shell, job, process);
 	std_fd_restore(old_fds);
 	if (shell->interactive == true)
-		tcsetpgrp(STDIN_FILENO, shell->pgid);
+		tcsetpgrp(STDERR_FILENO, shell->pgid);
 	return (ret);
 }

src/main.c
@@ -75,14 +75,14 @@ int				main(int argc, char **argv)
 		if (redir_file_argument(argv[1]) != 0)
 			return (1);
 	}
-	old_term_pgrp = tcgetpgrp(STDIN_FILENO);
-	interactive = isatty(STDIN_FILENO);
+	old_term_pgrp = tcgetpgrp(STDERR_FILENO);
+	interactive = (isatty(STDIN_FILENO) && isatty(STDERR_FILENO));
 	shell = init_shell(interactive);
 	if (shell == NULL)
 		return (1);
 	cetushell(shell);
 	exit_code = get_exit_code(shell);
-	tcsetpgrp(STDIN_FILENO, old_term_pgrp);
+	tcsetpgrp(STDERR_FILENO, old_term_pgrp);
 	free_shell(shell, 1);
 	return (exit_code);
 }

src/terminal_input/configure_terminal.c
@@ -19,14 +19,14 @@ static void		set_terminal_ownership(t_shell *shell)
 {
 	shell->pid = getpid();
 	shell->pgid = getpgid(shell->pid);
-	while (tcgetpgrp(STDIN_FILENO) != shell->pgid)
+	while (tcgetpgrp(STDERR_FILENO) != shell->pgid)
 	{
 		kill(-1 * shell->pgid, SIGTTIN);
 		shell->pgid = getpgid(shell->pid);
 	}
 	setpgid(shell->pid, shell->pid);
 	shell->pgid = shell->pid;
-	tcsetpgrp(STDIN_FILENO, shell->pgid);
+	tcsetpgrp(STDERR_FILENO, shell->pgid);
 }
 
 static void		set_terminal_attr(t_shell *shell, struct termios *orig)
@@ -45,7 +45,7 @@ static void		set_terminal_attr(t_shell *shell, struct termios *orig)
 	shell_temp.c_lflag &= ~(ECHO | ICANON);
 	shell_temp.c_cc[VMIN] = 1;
 	shell_temp.c_cc[VTIME] = 0;
-	tcsetattr(STDIN_FILENO, TCSADRAIN, &shell_temp);
+	tcsetattr(STDERR_FILENO, TCSADRAIN, &shell_temp);
 	shell->term = shell_temp;
 }
 
@@ -55,7 +55,7 @@ void			configure_terminal(t_shell *shell, int activator)
 
 	if (activator == 1 || activator == 2)
 	{
-		tcgetattr(STDIN_FILENO, &orig);
+		tcgetattr(STDERR_FILENO, &orig);
 	}
 	if (activator > 1)
 	{
@@ -64,6 +64,6 @@ void			configure_terminal(t_shell *shell, int activator)
 	}
 	if (activator == 0)
 	{
-		tcsetattr(STDIN_FILENO, TCSADRAIN, &orig);
+		tcsetattr(STDERR_FILENO, TCSADRAIN, &orig);
 	}
 }

src/terminal_input/get_cursor.c
@@ -44,13 +44,13 @@ void		get_cursor_pos(t_cursor *cursor, size_t prompt_len)
 	if (cursor == NULL)
 		return ;
 	send_terminal("sc");
-	ft_printf("%c[6n", ESCAPE_KEY);
+	ft_dprintf(STDERR_FILENO, "%c[6n", ESCAPE_KEY);
 	ft_memset(&pos, '\0', sizeof(pos));
-	ret = read(STDIN_FILENO, &pos, sizeof(pos));
+	ret = read(STDERR_FILENO, &pos, sizeof(pos));
 	if (ret == -1)
 		return ;
 	if (ret < 3)
-		read(STDIN_FILENO, &pos, sizeof(pos));
+		read(STDERR_FILENO, &pos, sizeof(pos));
 	else
 		parse_cursor_pos(cursor, pos, prompt_len, ret);
 	cursor->current.x = prompt_len;

src/utilities/term_utils.c
@@ -32,7 +32,7 @@ void		get_winsize(t_cursor *cursor, size_t len)
 	if (cursor == NULL)
 		return ;
 	get_cursor_pos(cursor, len);
-	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &winsize) == 0)
+	if (ioctl(STDERR_FILENO, TIOCGWINSZ, &winsize) == 0)
 	{
 		cursor->max.x = (size_t)winsize.ws_col;
 		cursor->max.y = (size_t)winsize.ws_row;

commit de4fcd56c7e348899da6858405de2fbd231dd931

fix segfault on selection to the right
This regresion was introduced in 13e6ac0 ("update output fd of cetushell"),
where a & was dropped when making unrelated changes. This caused a char
to be send to printf while a string was expected.

src/terminal_input/prompt.c
@@ -26,7 +26,7 @@ static void		print_buffer(t_buff *buffer)
 		ft_dprintf(STDERR_FILENO, "%.*s", buffer->rv_end - buffer->rv_start,
 		&buffer->buff[buffer->rv_start]);
 		ft_dprintf(STDERR_FILENO, "%s%s", RV_RESET,
-												buffer->buff[buffer->rv_end]);
+												&buffer->buff[buffer->rv_end]);
 	}
 	else if (buffer->rv_end < buffer->rv_start)
 	{